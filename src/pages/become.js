460f41201b68f0d3ede58f1eed83e84679e7f9d7
Jakalgit <mistetsmit05@gmail.com>
2023-04-04 16:11:42 +0300

    commit

---
+++ src/pages/become.js
@@ -0,0 +1,54 @@
+import style_fb from "@/styles/pages/feedback.module.css"
+import style from "@/styles/pages/become.module.css"
+import Grid from "@/components/Grid";
+import Image from "next/image";
+import {motion} from "framer-motion";
+
+const Become = () => {
+    return (
+        <div className="height" style={{display: 'flex', flexDirection: 'column'}}>
+            <Grid>
+                <div className={style_fb.pd}>
+                    <h1 className={style_fb.head}>Аккаунт создателя</h1>
+                    <p className={style_fb.text + ' ' + style_fb.txt_mt}>
+                        Продавайте свои музыкальные произведения, собирайте звезды чтобы оказаться в списках лучших,
+                        станьте одним из самых
+                        продаваемых бит-мейкеров, а мы вам поможем!
+                    </p>
+                    <div className={style.line + ' ' + style.stars}>
+                        <Image src={require("@/img/star.png")} alt="img" className={style.star}/>
+                        <Image src={require("@/img/star.png")} alt="img" className={style.star}/>
+                        <Image src={require("@/img/star.png")} alt="img" className={style.star}/>
+                        <Image src={require("@/img/star.png")} alt="img" className={style.star}/>
+                        <Image src={require("@/img/star.png")} alt="img" className={style.star}/>
+                    </div>
+                    <div className={style_fb.text}>
+                        Для того чтобы продолжить, требуестя подтвердить ваш номер телефона и пароль,
+                        на них придут коды которые нужно ввести на сайте. Вводите свои достоверные данные,
+                        так мы сможем вам помочь при утере пароля.  После регистрации вам потребуется оплатить
+                        регистрационный сбор в размере <p className={style_fb.bold}>389 ₽</p>.
+                    </div>
+                    <div className={style.dates}>
+                        <p className={style.data}>+7(***)-***-26-96</p>
+                        <p className={style.data + ' ' + style.danger}>E-Mail не указан</p>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div className={style.line}>
+                        <div className={style_fb.button_div + ' ' + style.btn_mt}>
+                            <motion.button
+                                className={style_fb.button}
+                                whileTap={{scale: 0.95}}
+                            >
+                                Подтвердить
+                            </motion.button>
+                        </div>
+                    </div>
+                </div>
+            </Grid>
+        </div>
+    );
+};
+
+export default Become;
--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -1,11 +1,34 @@
 import Grid from "@/components/Grid";
 import style from "@/styles/pages/feedback.module.css"
+import {motion} from "framer-motion";
 
 export default function Feedback() {
     return (
         <div className="height" style={{display: 'flex', flexDirection: 'column'}}>
             <Grid>
-                <h1 className={style.head}>Обратная связь</h1>
+                <div className={style.pd}>
+                    <h1 className={style.head}>Обратная связь</h1>
+                    <p className={style.text + ' ' + style.txt_mt}>
+                        Если у вас возникли проблемы при работе сайтом, вы можете написать их здесь, напишите в подробностях
+                    </p>
+                    <div className={style.text}>
+                        что у вас не работает, вы можете написать
+                        на почту <p className={style.bold}>beatmarket@yandex.ru</p>, или
+                        посмотрите <p className={style.href}>часто задаваемые вопросы</p>.
+                    </div>
+                    <p className={style.text + ' ' + style.txt_mt}>
+                        Так же мы рассмотрим ваши пожелания и рекомендации по улучшению проекта.
+                    </p>
+                    <textarea className={style.textarea} placeholder="Введите текст..." />
+                    <div className={style.button_div}>
+                        <motion.button
+                            className={style.button}
+                            whileTap={{scale: 0.97}}
+                        >
+                            Отправить
+                        </motion.button>
+                    </div>
+                </div>
             </Grid>
         </div>
     );
