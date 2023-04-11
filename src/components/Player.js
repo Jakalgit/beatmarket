a3381b2a3c6f5aba80877dc16afe5da1db2ddcdd
Jakalgit <mistetsmit05@gmail.com>
2023-04-11 12:10:19 +0300

    commit

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,22 @@
 import style from "@/styles/components/player.module.css"
 import {motion} from "framer-motion";
-import {useState} from "react";
+import {useRef, useState} from "react";
+import Grid from "@/components/Grid";
+import Image from "next/image";
 
 const Player = () => {
 
     const [visible, setVisible] = useState(true)
+
+    const wrapperRef = useRef(null)
+    const trackRef = useRef(null)
+
+    const timeClick = (e) => {
+        const x = e.pageX
+        const width = wrapperRef.current.offsetWidth
+        const trackW = (x * 100) / width
+        trackRef.current.style.width = trackW + "%"
+    }
 
     return (
         <>
@@ -17,13 +29,39 @@ const Player = () => {
                     x: visible ? 0 : "-7.5rem"
                 }}
             >
-                <div className={style.wrapper}>
-                    <input
+                <motion.div
+                    className={style.wrapper}
+                    ref={wrapperRef}
+                    onClick={(e) => timeClick(e)}
+                >
+                    <div
                         className={style.range}
-                        type="range"
-                        min={0}
-                        max={200}
+                        ref={trackRef}
                     />
+                </motion.div>
+                <div className={style.controls}>
+                    <Grid>
+                        <div className={style.line}>
+                            <div className={style.part + ' ' + style.one}>
+                                <Image className={style.preview} alt="preview" src={require("@/img/preview.png")} />
+                                <div className={style.nameBlock}>
+                                    <div className={style.name}>
+                                        OVERHEAVEN (Synthware)
+                                        <p className={style.bpm}>170 bpm</p>
+                                    </div>
+                                    <p className={style.author}>
+                                        GONE.Fludd
+                                    </p>
+                                </div>
+                            </div>
+                            <div className={style.part + ' ' + style.two}>
+
+                            </div>
+                            <div className={style.part + ' ' + style.three}>
+
+                            </div>
+                        </div>
+                    </Grid>
                 </div>
             </motion.div>
         </>
